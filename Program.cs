9865b73c22cb26475c3c74967e1a3c573ec9938a
TaHuC <TaHuC@hotmail.com>
2017-01-30 23:20:27 +0200

    Creater two values complete

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace CreaterTwoValues
+{
+	class MainClass
+	{
+		public static void Main()
+		{
+			var type = Console.ReadLine();
+
+			if (type == "int")
+			{
+				int firstValue = int.Parse(Console.ReadLine());
+				int secondValue = int.Parse(Console.ReadLine());
+
+				Console.WriteLine(GetMax(firstValue, secondValue));
+				return;
+			}
+			else if (type == "char")
+			{
+				char firstValue = char.Parse(Console.ReadLine());
+				char secondValue = char.Parse(Console.ReadLine());
+
+				Console.WriteLine(GetMax(firstValue, secondValue));
+				return;
+
+			}
+			else
+			{
+				var firstValue = Console.ReadLine();
+				var secondValue = Console.ReadLine();
+
+				Console.WriteLine(GetMax(firstValue, secondValue));
+				return;
+			}
+
+
+		}
+
+		public static int GetMax(int firstVal, int secondVal)
+		{
+			return Math.Max(firstVal, secondVal);
+		}
+
+		public static char GetMax(char firstVal, char secondVal)
+		{
+			return (char)GetMax((int)firstVal, (int)secondVal);
+		}
+
+		public static string GetMax(string firstVal, string secondVal)
+		{
+			if (firstVal.CompareTo(secondVal) < 0)
+			{
+				return secondVal;
+			}
+			else
+			{
+				return firstVal;
+			}
+		}
+
+	}
+}
